ff23a45b78e223b96798628039a4b3b15ef20984
moadan <mohamed_adan@hotmail.co.uk>
2023-06-14 15:55:56 +0100

    "p tag @media view on small"

---
+++ database.js
@@ -0,0 +1,24 @@
+/* document.write(Date());
+
+
+
+function tellTime() {
+    let dayNames = ["sun", "mon", "tue", "wed", "thu", "fri", "sat",];
+    let now = new Date();
+    let theDay = now.getDay();
+    let nameOfToday = dayNames[theDay];
+    alert(nameOfToday);
+} 
+
+tellTime(); 
+
+function getTime() {
+    let now1 = new Date();
+    let hours = now1.getHours();
+    let minutes = now1.getMinutes();
+    alert("Current time: " + hours + ":" + minutes);
+}
+
+getTime(); 
+
+ */
